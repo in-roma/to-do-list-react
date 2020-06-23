67cd642cdab448c8cdaa8fed3304e2db4779e604
Romain GRANDJEAN <romaingrandjean@Romains-Air.fritz.box>
2020-06-23 21:02:53 +0200

    version with onchange

--- src/components/Taskfield.jsx
+++ src/components/Taskfield.jsx
@@ -4,24 +4,33 @@ class Taskfield extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          userInput: ""
+          userInput: "",
+          list: []
         };
       }
 
     render() { 
+        this.changeHandel = (e) => {
+            console.log(e.target.value);
+            this.setState({ userInput: e.target.value.trim() });
+          };
 
         this.add = (e) => {
             const userText = this.state.userInput;
             this.setState({
-                userInput: ""
-            });
+                userInput: "",
+                list: [
+                    ...this.state.list,
+                    { id: this.state.list.length, title: userText, done: false },
+                  ],
+                });
         };
         console.log(this.userInput);
 
         return <div className="add-to-section">
                     <div className="add-todo">
                         <div className="text-input">
-                            <input type="text" id="userText" value={this.state.userInput} placeholder="Type a task here and click + to add"/>
+                            <input type="text" id="userText" value={this.state.userInput} onChange={this.changeHandel} placeholder="Type a task here and click + to add"/>
                             <div className="underline"></div>
                         </div>
                         <button className="buttonAdd" onClick="add" ><img src="./img/add-button.svg" alt=""/></button>
