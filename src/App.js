e11775f07892842c51a2dff498617708b6d3e810
Romain GRANDJEAN <romaingrandjean@Romains-MacBook-Air.local>
2020-06-23 14:08:00 +0200

    setup - complete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    render() { 
+
+        return (
+            <React.Fragment>
+                <p>Hello</p>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Frameworks Modules
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// JS & CSS Modules 
+import './scss/main.scss';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
