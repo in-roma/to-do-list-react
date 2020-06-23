5edc8123605ddbadbc26fafc1683615e48eb2f43
Romain GRANDJEAN <romaingrandjean@Romains-MacBook-Air.local>
2020-06-23 14:45:18 +0200

    version with full structure & first components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './components/Header'
 
 class App extends React.Component {
 
@@ -6,7 +7,7 @@ class App extends React.Component {
 
         return (
             <React.Fragment>
-                <p>Hello</p>
+                <Header />
             </React.Fragment>
         );
     }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() { 
+        return <div className="logo">
+        <img src="./img/logo.svg" alt=""/>
+        </div>;
+    }
+}
+ 
+export default Header;                
+
+
