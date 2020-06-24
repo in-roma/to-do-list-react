91858374b0b6d803d7945e51e9d08d33fb407896
Romain GRANDJEAN <romaingrandjean@Romains-Air.fritz.box>
2020-06-24 21:48:39 +0200

    version with adding working

--- src/components/Taskfield.jsx
+++ src/components/Taskfield.jsx
@@ -8,33 +8,39 @@ class Taskfield extends React.Component {
           list: []
         };
       }
+      changeHandel = (e) => {
+        this.setState({ ...this.state, userInput: e.target.value.trim() });
+        console.log("this is e.target.value", e.target.value);
+      };
 
+      add = (e) => {
+        const userText = this.state.userInput;
+        this.setState({
+            userInput: "",
+            list: [
+                ...this.state.list,
+                { id: this.state.list.length, title: userText, done: false },
+              ],
+            });
+            console.log(this.state);
+    };
+    
     render() { 
-        this.changeHandel = (e) => {
-            this.setState({ userInput: e.target.value.trim() });
-          };
-
-        this.add = (e) => {
-            const userText = this.state.userInput;
-            this.setState({
-                userInput: "",
-                list: [
-                    ...this.state.list,
-                    { id: this.state.list.length, title: userText, done: false },
-                  ],
-                });
-        };
-        console.log(this.state.userInput);
-
-        return <div className="add-to-section">
-                    <div className="add-todo">
-                        <div className="text-input">
-                            <input type="text" id="userText" value={this.state.userInput} onChange={this.changeHandel} placeholder="Type a task here and click + to add"/>
-                            <div className="underline"></div>
-                        </div>
-                        <button className="buttonAdd" onClick="add" ><img src="./img/add-button.svg" alt=""/></button>
-                    </div>
+        
+        return <React.Fragment>
+                <div className="add-to-section">
+                      <div className="add-todo">
+                          <div className="text-input">
+                              <input type="text" id="userText" value={this.state.userInput} onChange={this.changeHandel} placeholder="Type a task here and click + to add"/>
+                              <div className="underline"></div>
+                          </div>
+                          <button className="buttonAdd" onClick={this.add} ><img src="./img/add-button.svg" alt=""/></button>
+                      </div>
                 </div>
+                      <div className="tasks"> 
+                          {this.state.list.map(item => <div className="divResult"><div className="tasksText">{item.title}</div></div>)}
+                      </div>
+              </React.Fragment> 
     }
 }
 export default Taskfield;                
